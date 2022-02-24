5a76b6c8d72a76e7118a23124d296f6e24870ee5
Ana <ana.chacon@travelperk.com>
2022-02-24 18:26:48 +0100

    Define recipe and ingredient models

--- app/recipe/models.py
+++ app/recipe/models.py
@@ -1,3 +1,23 @@
+from unicodedata import name
 from django.db import models
 
-# Create your models here.
+
+class Recipe(models.Model):
+    """Recipe object"""
+
+    # id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    """Ingredient object"""
+
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Recipe
 
 
 def index(request):
-    return HttpResponse("Hello")
+    allRecipeNames = Recipe.objects.all()
+    print(type(allRecipeNames))
+    allRecipeNamesList = list(allRecipeNames)
+    print(allRecipeNamesList)
+    print(allRecipeNamesList[0])
+    # for e in allRecipeNames:
+    #     print(e.description)
+
+    # print(type(allRecipeNames))
+    # insideObject = allRecipeNames.description
+    return HttpResponse("hola")
