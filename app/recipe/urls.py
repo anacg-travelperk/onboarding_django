78bcefde9b2f1ea68112bb4aec130ce7df615512
Ana <ana.chacon@travelperk.com>
2022-03-01 12:03:11 +0100

    Cleanup

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -1,13 +1,8 @@
-# from xml.etree.ElementInclude import include
 from django.urls import path, include
 from rest_framework import routers
 
 from recipe import views
 
-# urlpatterns = [
-#     path("", views.index, name="index"),
-#     # path("<int:name>/", views.recipecontent, name="recipecontent"),
-# ]
 
 router = routers.DefaultRouter()
 router.register("", views.RecipeViewSet)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,39 +1,10 @@
-# from django.shortcuts import render
-# from django.http import HttpResponse, JsonResponse
 from .models import Recipe
 from .serializers import RecipeSerializer
-
 from rest_framework import viewsets
-
-# from rest_framework.response import Response
-
-# from rest_framework.decorators import api_view, renderer_classes
-# from rest_framework.renderers import JSONRenderer, TemplateHTMLRenderer
-
-# @api_view(("GET",))
-# @renderer_classes((TemplateHTMLRenderer, JSONRenderer))
-# def index(request):
-#     allRecipeNames = Recipe.objects.all()
-#     serializer = RecipeSerializer(allRecipeNames)
-#     # print(type(allRecipeNames))
-#     # allRecipeNamesList = list(allRecipeNames)
-#     # print(allRecipeNamesList)
-#     # print(allRecipeNamesList[0])
-#     # for e in allRecipeNames:
-#     #     print(e.description)
-
-#     # print(type(allRecipeNames))
-#     # insideObject = allRecipeNames.description
-#     return Response(allRecipeNames, template_name=None)
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
     """Manage recipes in the database"""
 
-    # authentication_classes = (TokenAuthentication,)
-    # permission_classes = (IsAuthenticated,)
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
-    # def get_queryset(self):
-    #     """Return objects for the current authenticated user only"""
-    #     return self.queryset.order_by("-name")
