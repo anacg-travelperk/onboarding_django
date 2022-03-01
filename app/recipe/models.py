a802b0765efd3e363b643b14f5aa208e381381e4
Ana <ana.chacon@travelperk.com>
2022-03-01 14:47:02 +0100

    Add Ingredients serializer

--- app/recipe/models.py
+++ app/recipe/models.py
@@ -5,7 +5,6 @@ from django.db import models
 class Recipe(models.Model):
     """Recipe object"""
 
-    # id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
 
@@ -13,11 +12,13 @@ class Recipe(models.Model):
         return self.name
 
 
-# class Ingredient(models.Model):
-#     """Ingredient object to be used in a recipe"""
+class Ingredient(models.Model):
+    """Ingredient object to be used in a recipe"""
 
-#     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
-#     name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    recipe = models.ForeignKey(
+        Recipe, related_name="ingredients", on_delete=models.CASCADE
+    )
 
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,15 +1,19 @@
 from rest_framework import serializers
-from .models import Recipe
+from .models import Recipe, Ingredient
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model: Ingredient
+        fields = "__all__"
 
 
 class RecipeSerializer(serializers.ModelSerializer):
+    ingredients = IngredientSerializer(read_only=True, many=True)
+
     class Meta:
         model = Recipe
-        fields = [
-            "id",
-            "name",
-            "description",
-        ]
+        fields = "__all__"
         read_only_fields = [
             "id",
         ]
