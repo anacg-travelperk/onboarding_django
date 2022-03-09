51541956a53d317ffed0dd74a0b2833055e3d61d
Ana <ana.chacon@travelperk.com>
2022-03-09 18:45:55 +0100

    Add patch and post endpoints

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -5,15 +5,42 @@ from .models import Recipe, Ingredient
 class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ingredient
-        fields = "__all__"
+        # fields = "__all__"
+        fields = ("name",)
 
 
 class RecipeSerializer(serializers.ModelSerializer):
-    ingredients = IngredientSerializer(read_only=True, many=True)
+    ingredients = IngredientSerializer(read_only=False, many=True)
 
     class Meta:
         model = Recipe
-        fields = "__all__"
+        fields = (
+            "id",
+            "name",
+            "description",
+            "ingredients",
+        )
         read_only_fields = [
             "id",
         ]
+
+    def create(self, validated_data):
+        recipe = Recipe.objects.create(
+            name=validated_data["name"], description=validated_data["description"]
+        )
+        ingredients_data = validated_data["ingredients"]
+
+        if ingredients_data:
+            for ingredient in ingredients_data:
+                Ingredient.objects.create(recipe=recipe, name=ingredient["name"])
+        return recipe
+
+    def update(self, instance, validated_data):
+        recipe = instance
+        ingredients_data = validated_data["ingredients"]
+
+        if ingredients_data:
+            recipe.ingredients.all().delete()
+            for ingredient in ingredients_data:
+                Ingredient.objects.create(recipe=recipe, name=ingredient["name"])
+        return recipe
