f6c2abd046ac13a42c0d91ae08adddfeac5c90bd
Ana <ana.chacon@travelperk.com>
2022-03-11 09:57:31 +0100

    Remove unused mock data

--- app/recipe/tests/test_serializers.py
+++ app/recipe/tests/test_serializers.py
@@ -16,19 +16,6 @@ def sample_recipe():
     return recipe
 
 
-def sample_invalid_recipe():
-    """Create and return a invalid sample recipe"""
-
-    defaults = {
-        "name": 12,
-        "description": 32,
-    }
-    recipe = Recipe.objects.create(**defaults)
-    recipe.ingredients.create(name="ingredient1")
-    recipe.ingredients.create(name="ingredient2")
-    return recipe
-
-
 class SerializersTests(TestCase):
     def test_serializer_valid_recipe(self):
         recipe = sample_recipe()
